02c7c037fda17479fc2d4473ab645139f1cb40f8
artyeug <artyeug@gmail.com>
2018-04-10 00:33:48 +0700

    Implement a computer's logic in the main method and add an auxiliary method

--- src/Computer.java
+++ src/Computer.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Computer extends CardPlayer {
     private static int _computerIndex;
 
@@ -14,9 +16,57 @@ public class Computer extends CardPlayer {
         return _computerIndex;
     }
 
+    private TurnStatement calculatePossibility(double percentage) {
+        Random random = new Random();
+        int maxPossibility = 100;
+
+        int possibility = random.nextInt(maxPossibility);
+        if (possibility <= percentage)
+            return TurnStatement.HIT;
+        else
+            return TurnStatement.STAND;
+    }
+
     @Override
-    public TurnAnswer analyzeTurn() throws Exception  {
-        // here is a computer logic..
-        return TurnAnswer.PASS; // tmp
+    public TurnStatement analyzeTurn() {
+        String playerName = this.getPlayerName();
+        int pointsAmount = this.getPointsAmount();
+
+        if (pointsAmount > TwentyOnePoints.MAX_SCORE) {
+            System.out.println(playerName + " has exceeded the maximum score. It's lost.");
+            return TurnStatement.EXCEED;
+        }
+        else if (pointsAmount == TwentyOnePoints.MAX_SCORE) {
+            System.out.println(playerName + " has earned 21 points!");
+            return TurnStatement.WIN;
+        }
+
+        if (this.isDealer() && pointsAmount >= TwentyOnePoints.MAX_DEALER_TOTAL)
+            return TurnStatement.STAND;
+
+        // Below is the main decision determination:
+
+        int upperBound = TwentyOnePoints.MAX_SCORE - TwentyOnePoints.MIN_WEIGHT + 1;
+        int lowerBound = TwentyOnePoints.MAX_SCORE - TwentyOnePoints.MAX_WEIGHT;
+
+        if (pointsAmount == upperBound)
+            return TurnStatement.STAND;
+
+        if (pointsAmount <= lowerBound)
+            return TurnStatement.HIT;
+
+        int firstBreakpoint = lowerBound + (upperBound - lowerBound) / 5;
+        int secondBreakpoint = lowerBound + (upperBound - lowerBound) / 2;
+        int thirdBreakpoint = upperBound - 1;
+
+        if (pointsAmount <= firstBreakpoint)
+            return calculatePossibility(85);
+        else if (pointsAmount <= secondBreakpoint)
+            return calculatePossibility(60);
+        else if (pointsAmount <= thirdBreakpoint)
+            return calculatePossibility(35);
+
+
+        return TurnStatement.STAND;
     }
 }
