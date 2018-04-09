357a0354740aedcfb36cc9cfdaded0e3c13e0713
artyeug <artyeug@gmail.com>
2018-04-10 00:31:54 +0700

    Bring the OperatingSystem class here and split code into several methods

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,51 @@
 import javax.swing.*;
 
+final class OperatingSystem {
+    public static final String OS_NAME = System.getProperty("os.name");
+
+    private OperatingSystem() {
+        System.out.println("There is nothing to construct.");
+    }
+
+    static String getLookAndFeel() {
+        String osNameLowerCase = OS_NAME.toLowerCase();
+
+        if (osNameLowerCase.contains("windows"))
+            return "com.sun.java.swing.plaf.windows.WindowsLookAndFeel";
+        else if (osNameLowerCase.contains("linux"))
+            return "com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
+        else
+            return "javax.swing.plaf.metal.MetalLookAndFeel"; // too bad
+    }
+}
+
 public class Main {
-    public static void main(String[] args) throws Exception {
+    private static void runConsoleApplication() throws Exception {
+        TwentyOnePoints top = new TwentyOnePoints();
+        top.playGame();
+    }
+
+    private static void runGuiApplication() throws Exception {
         UIManager.setLookAndFeel(OperatingSystem.getLookAndFeel());
 
         TwentyOnePointsGui top = new TwentyOnePointsGui();
 
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    top.windowAssembly();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        SwingUtilities.invokeLater(() -> {
+            try {
+                top.windowAssembly();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
+    }
+
+    public static void main(String[] args) {
+        try {
+//            runGuiApplication();
+            runConsoleApplication();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
