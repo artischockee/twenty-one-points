2ebc4104714de6ed7fe110cfa82467e5164b61f8
artyeug <artyeug@gmail.com>
2018-04-11 22:51:20 +0700

    Set up the MVC pattern

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
-import view.TwentyOnePointsGui;
+import controller.Controller;
+import model.GameModel;
+import view.Application;
 
 import javax.swing.*;
 
@@ -7,10 +9,16 @@ public class Main {
         try {
             UIManager.setLookAndFeel(OperatingSystem.getLookAndFeel());
 
-            TwentyOnePointsGui top = new TwentyOnePointsGui();
-
             SwingUtilities.invokeLater(() -> {
-                top.windowAssembly();
+                try {
+                    GameModel model = new GameModel();
+                    Application view = new Application(model);
+                    new Controller(model, view);
+
+                    view.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             });
         }
         catch(Exception e) {
