69b4b638d9f2c26ea22cbbe49991428af0c93e8e
artyeug <artyeug@gmail.com>
2018-04-12 13:46:19 +0700

    Split some code into methods; add hardly supported code (that will be deleted soon)

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -31,10 +31,66 @@ public class Controller {
         private void invokeGameRoutine() {
             _model.initialActions();
 
+            // Not code, just piece of sheet:
+
+            int layerOffset = _view.getCardPlayerPaneOffset();
+            for (int i = 0; i < _model.getCardPlayers().size(); ++i) {
+                Card card = _model.getCardPlayer(i).getCardDeck().lastElement();
+
+                int layerBoundX = 0;
+                int layerBoundY = 0;
+                int layerPosition = 0;
+
+                if (i == 0) {
+                    layerBoundX = _view.getDealerPaneBoundX();
+                    layerBoundY = _view.getDealerPaneBoundY();
+                    layerPosition = _view.getDealerPaneLayerPosition();
+                }
+                else if (i == 1) {
+                    layerBoundX = _view.getPlayerPaneBoundX();
+                    layerBoundY = _view.getPlayerPaneBoundY();
+                    layerPosition = _view.getPlayerPaneLayerPosition();
+                }
+
+                String cardName = card.getCardNameSymbol();
+                String cardSuit = card.getSuitShortNotation();
+                ImageIcon cardImage = new ImageIcon("images/cards/" + cardName + cardSuit + ".png");
+                JLabel label = new JLabel(cardImage);
+                label.setBounds(layerBoundX, layerBoundY, cardImage.getIconWidth(), cardImage.getIconHeight());
+
+                if (i == 0) {
+                    _view.getDealerPane().add(label, Integer.valueOf(layerPosition));
+                    _view.setDealerPaneLayerPosition(++layerPosition);
+                    _view.setDealerPaneBoundX(layerBoundX + layerOffset);
+                }
+                else if (i == 1) {
+                    _view.getPlayerPane().add(label, Integer.valueOf(layerPosition));
+                    _view.setPlayerPaneLayerPosition(++layerPosition);
+                    _view.setPlayerPaneBoundX(layerBoundX + layerOffset);
+                }
+            }
+
+            this.refreshDynamicFields();
 
             _view.getCardDeckButton().setEnabled(true);
             _view.getHitButton().setEnabled(true);
             _view.getStandButton().setEnabled(true);
+        }
+
+        private void launchApplication() {
+            _view.getMainPanel().remove(_view.getInitialPanel());
+            _view.windowAssembly();
+        }
+
+        private void refreshDynamicFields() {
+            _view.getDealerTotalPtsLabel().setText(
+                    Integer.toString(_model.getCardPlayer(0).getPointsAmount()));
+
+            _view.getPlayerTotalPtsLabel().setText(
+                    Integer.toString(_model.getCardPlayer(1).getPointsAmount()));
+
+            _view.getCardDeckSizeLabel().setText(
+                    Integer.toString(_model.getCardDeckSize()));
         }
 
         private void showExitDialog() {
@@ -42,6 +98,7 @@ public class Controller {
                     "Are you sure you want to quit the game?",
                     "Game quit",
                     JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
             switch (answer) {
                 case 0:
                     System.exit(0);
@@ -51,24 +108,33 @@ public class Controller {
             }
         }
 
+        private void showAboutDialog() {
+            JOptionPane.showMessageDialog(
+                    _view,
+                    "Twenty-One Points Game.\nAuthor: Artem Piskarev",
+                    "About", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        private void showEmptyDeckWarningDialog() {
+            JOptionPane.showMessageDialog(
+                    _view, "There are no cards left on the deck!",
+                    "The card deck is empty",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+
         private void addCardFromDeck() {
             if (_model.getCardDeck().isEmpty()) {
-                JOptionPane.showMessageDialog(
-                        _view, "There are no cards left on the deck!",
-                        "The card deck is empty",
-                        JOptionPane.WARNING_MESSAGE
-                );
+                this.showEmptyDeckWarningDialog();
                 return;
             }
 
             Card card = _model.getCardDeck().pop();
 
-            // .. update carddeck label
-            _view.getCardDeckSizeLabel().setText(Integer.toString(_model.getCardDeckSize()) + " cards");
+            _view.getCardDeckSizeLabel().setText(Integer.toString(_model.getCardDeckSize()));
 
             int layerBoundX = _view.getPlayerPaneBoundX();
             int layerBoundY = _view.getPlayerPaneBoundY();
-            int layerOffset = _view.getPlayerPaneOffset();
+            int layerOffset = _view.getCardPlayerPaneOffset();
             int layerPosition = _view.getPlayerPaneLayerPosition();
 
             String cardName = card.getCardNameSymbol();
@@ -91,29 +157,24 @@ public class Controller {
 
             switch (command) {
                 case NEW_GAME:
+                    // TODO: 4/12/18 Check if the game is already played
                     this.invokeGameRoutine();
                     break;
                 case EXIT_GAME:
                     this.showExitDialog();
                     break;
                 case LAUNCH_APP:
-                    _view.getMainPanel().remove(_view.getInitialPanel());
-                    _view.windowAssembly();
+                    this.launchApplication();
                     break;
                 case HIT:
                     this.addCardFromDeck();
                     break;
                 case STAND:
-                    // tmp impl:
-                    System.out.println(_model.getCardDeckSize());
-                    System.out.println(_model.getCardPlayer(1).getPointsAmount());
+                    // ..
                     break;
                 case ABOUT:
-                    JOptionPane.showMessageDialog(_view,
-                            "Twenty-One Points Game.\nAuthor: Artem Piskarev",
-                            "About", JOptionPane.INFORMATION_MESSAGE);
+                    this.showAboutDialog();
                     break;
-                // ...
             }
         }
     }
