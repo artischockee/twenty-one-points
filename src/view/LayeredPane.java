8c19c23858c811fb26dd2571477476a0783b5f1d
artyeug <artyeug@gmail.com>
2018-04-14 02:08:55 +0700

    Add 'reset' method

--- src/view/LayeredPane.java
+++ src/view/LayeredPane.java
@@ -3,11 +3,26 @@ package view;
 import javax.swing.*;
 
 public class LayeredPane extends JLayeredPane {
+    // Attributes ::
+
     public static final int OFFSET = 30;
 
     private int _boundx;
     private int _boundy;
     private int _layerPosition;
+
+    // Constructor ::
+
+    public LayeredPane() {
+        super();
+
+        _boundx = 0;
+        _boundy = 0;
+        _layerPosition = 0;
+    }
+
+
+    // Accessors (get/set) ::
 
     public int getBoundX() {
         return _boundx;
@@ -33,8 +48,13 @@ public class LayeredPane extends JLayeredPane {
         _layerPosition = position;
     }
 
-    public LayeredPane() {
-        super();
+
+    // Methods ::
+
+    public void reset() {
+        this.removeAll();
+        this.revalidate();
+        this.repaint();
 
         _boundx = 0;
         _boundy = 0;
