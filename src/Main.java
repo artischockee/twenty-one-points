33068d9396e246d77640242613bb5b50c949bb10
artyeug <artyeug@gmail.com>
2018-04-25 22:03:54 +0700

    Extract nested try-catch block; remove GameModel local variable due to its uselessness

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,21 @@ public class Main {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(OperatingSystem.getLookAndFeel());
-
-            SwingUtilities.invokeLater(() -> {
-                try {
-                    GameModel model = new GameModel();
-                    Application view = new Application(model, new Locale("en", "US"));
-//                    Application view = new Application(model, new Locale("ru", "RU"));
-                    new Controller(model, view);
-
-                    view.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
+
+        SwingUtilities.invokeLater(() -> {
+            try {
+                Application view = new Application(new Locale("en", "US"));
+//                    Application view = new Application(new Locale("ru", "RU"));
+                new Controller(new GameModel(), view);
+
+                view.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
