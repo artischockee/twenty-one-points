b3180a8562533da93b1cb8471dd7e8aa5380138c
artyeug <artyeug@gmail.com>
2018-04-25 14:56:03 +0700

    Rearrange the class and its fields

--- src/view/LayeredPane.java
+++ src/view/LayeredPane.java
@@ -5,56 +5,50 @@ import java.awt.*;
 
 public class LayeredPane extends JLayeredPane {
     public static final int OFFSET = 30;
-    public static final int DEFAULT_CARD_WIDTH = 84;
-    public static final int DEFAULT_CARD_HEIGHT = 128;
+    public static final Dimension DEFAULT_DIMENSION;
+    private static final int DEFAULT_CARD_WIDTH = 84;   // these fields should probably lie
+    private static final int DEFAULT_CARD_HEIGHT = 128; // in Card class, but who knows..
 
-    private int _boundx;
-    private int _boundy;
-    private int _layerPosition;
+    private int boundx;
+    private int boundy;
 
-
-    public LayeredPane() {
-        super();
-
-        _boundx = 0;
-        _boundy = 0;
-        _layerPosition = 0;
+    static {
+        DEFAULT_DIMENSION = new Dimension(DEFAULT_CARD_WIDTH, DEFAULT_CARD_HEIGHT);
     }
 
+    LayeredPane() {
+        boundx = 0;
+        boundy = 0;
+    }
 
     public int getBoundX() {
-        return _boundx;
-    }
-
-    public void setBoundX(int bound) {
-        _boundx = bound;
+        return boundx;
     }
 
     public int getBoundY() {
-        return _boundy;
+        return boundy;
     }
 
-    public void setBoundY(int bound) {
-        _boundy = bound;
+    @Override
+    public Component add(Component comp) {
+        this.addImpl(comp, null, 0);
+
+        boundx += OFFSET;
+
+        return comp;
     }
 
-    public int getLayerPosition() {
-        return _layerPosition;
+    public void increaseWidth() {
+        this.setPreferredSize(new Dimension(this.getWidth() + OFFSET, DEFAULT_CARD_HEIGHT));
     }
-
-    public void setLayerPosition(int position) {
-        _layerPosition = position;
-    }
-
 
     public void reset() {
         this.removeAll();
-        this.setPreferredSize(new Dimension(DEFAULT_CARD_WIDTH, DEFAULT_CARD_HEIGHT));
+        this.setPreferredSize(DEFAULT_DIMENSION);
         this.revalidate();
         this.repaint();
 
-        _boundx = 0;
-        _boundy = 0;
-        _layerPosition = 0;
+        boundx = 0;
+        boundy = 0;
     }
 }
